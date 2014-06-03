7ea4b15720d92856dc99e0cfbd30937b4ed2357c
Kevin Lange <klange@dakko.us>
2014-06-03 01:44:13 -0400

    print source -> mediacrush so it shows up in logs

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def index():
             if not request.query_string.startswith("http:") and not request.query_string.startswith("https:"):
                 return app.send_static_file("invalid.htm")
             tmp_file = subprocess.check_output([SITE_URL + 'bin/tsundere.me', request.query_string]).strip()
+            print "[upload] %s -> %s" % (request.query_string, tmp_file)
             if tmp_file == "no faces":
                 return app.send_static_file("noface.htm")
             return redirect(tmp_file, code=302)
