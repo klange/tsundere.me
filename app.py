bfe2993393c9a050ed255340796b3619301db4a3
Kevin Lange <klange@dakko.us>
2014-06-03 03:30:14 -0400

    Throw a different error for bad images vs. no face

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ def index():
                 return app.send_static_file("invalid.htm")
             tmp_file = subprocess.check_output([SITE_URL + 'bin/tsundere.me', request.query_string]).strip()
             print "[upload] %s -> %s" % (request.query_string, tmp_file)
+            if tmp_file == "bad image":
+                return app.send_static_file("invalid.htm")
             if tmp_file == "no faces":
                 return app.send_static_file("noface.htm")
             return redirect(tmp_file, code=302)
