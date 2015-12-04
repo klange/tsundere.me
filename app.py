fe6a96702a1eb31a97e4484d2d6aae15c0aedd16
Kevin Lange <klange@dakko.us>
2015-12-04 14:19:09 -0500

    Different error for bad image

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
             tmp_file = subprocess.check_output([SITE_URL + 'bin/tsundere.me', request.query_string]).strip()
             print "[upload] %s -> %s" % (request.query_string, tmp_file)
             if tmp_file == "bad image":
-                return app.send_static_file("invalid.htm")
+                return app.send_static_file("badimage.htm")
             if tmp_file == "no faces":
                 return app.send_static_file("noface.htm")
             return redirect(tmp_file, code=302)
